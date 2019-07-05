fdec9a14ca2459a9dfe79ebb01c6194cfbb3f887
peterzernia <peter.zernia@medigo.com>
2019-07-05 15:05:22 +0200

    converted image to svg

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react'
 import {
-  Alert, AsyncStorage, StyleSheet, Text, View, Image, TouchableHighlight,
+  Alert, AsyncStorage, StyleSheet, Text, View, TouchableHighlight,
 } from 'react-native'
+import Svg, { Path } from 'react-native-svg'
 import axios from 'axios'
-
 import Voice from 'react-native-voice'
-import button from './button.png'
 
 const styles = StyleSheet.create({
-  button: {
-    width: 50,
-    height: 50,
-  },
   container: {
     flex: 1,
     justifyContent: 'space-between',
@@ -279,7 +274,10 @@ class VoiceTest extends Component {
           onPressIn={this._startRecognizing}
           onPressOut={this._stopRecognizing}
         >
-          <Image style={styles.button} source={button} />
+          <Svg width={24} height={24}>
+            <Path d="M12 14c1.66 0 2.99-1.34 2.99-3L15 5c0-1.66-1.34-3-3-3S9 3.34 9 5v6c0 1.66 1.34 3 3 3zm5.3-3c0 3-2.54 5.1-5.3 5.1S6.7 14 6.7 11H5c0 3.41 2.72 6.23 6 6.72V21h2v-3.28c3.28-.48 6-3.3 6-6.72h-1.7z" />
+            <Path d="M0 0h24v24H0z" fill="none" />
+          </Svg>
         </TouchableHighlight>
         {
           // <TouchableHighlight onPress={this._cancelRecognizing}>
